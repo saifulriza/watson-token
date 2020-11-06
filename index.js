4d813a2c7b7d71c16c0afde18faddf6d74379bbb
Restyled.io <commits@restyled.io>
2020-11-06 09:21:35 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -6,33 +6,36 @@ let token;
 function generateToken(url, path = "/", port = 80, apiKey, callback) {
   let response = [];
   if (url && apiKey && callback) {
-    if (typeof url == !"string") throw "url must be a string";
-    if (typeof path == !"string") throw "path must be a string";
-    if (typeof port == !"number") throw "port must be a number";
-    if (typeof apiKey == !"string") throw "apiKey must be a string";
-    if (typeof callback == !"function") throw "callback must be a function";
+    if (typeof url == !"string")
+      throw "url must be a string";
+    if (typeof path == !"string")
+      throw "path must be a string";
+    if (typeof port == !"number")
+      throw "port must be a number";
+    if (typeof apiKey == !"string")
+      throw "apiKey must be a string";
+    if (typeof callback == !"function")
+      throw "callback must be a function";
 
     const data = querystring.stringify({
-      grant_type: "urn:ibm:params:oauth:grant-type:apikey",
-      apikey: apiKey,
+      grant_type : "urn:ibm:params:oauth:grant-type:apikey",
+      apikey : apiKey,
     });
 
     const options = {
-      hostname: url,
-      path: path,
-      port: port,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Accept: "application/json",
-        "Content-Length": Buffer.byteLength(data),
+      hostname : url,
+      path : path,
+      port : port,
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/x-www-form-urlencoded",
+        Accept : "application/json",
+        "Content-Length" : Buffer.byteLength(data),
       },
     };
 
     const req = http.request(options, (res) => {
-      res.on("data", (chunk) => {
-        response.push(chunk);
-      });
+      res.on("data", (chunk) => { response.push(chunk); });
 
       res.on("end", () => {
         const buffer = Buffer.concat(response);
@@ -42,9 +45,7 @@ function generateToken(url, path = "/", port = 80, apiKey, callback) {
       });
     });
 
-    req.on("error", (error) => {
-      throw error;
-    });
+    req.on("error", (error) => { throw error; });
 
     req.write(data);
     req.end();
